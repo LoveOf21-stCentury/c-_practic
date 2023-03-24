248901989023637a13b9bdfefd1fe320f67868f6
Sylm@n <l.musilmanbekova12@gmail.com>
2023-03-24 19:51:25 +0300

    modified exmpl004 div

--- exmpl004_div/Program.cs
+++ exmpl004_div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numA = 12;
+double numB = 5;
+double result = numA / numB;
+Console.WriteLine(result);
