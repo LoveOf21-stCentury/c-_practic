663f82a7de0a96edf27044084bebfed19c21708f
Sylm@n <l.musilmanbekova12@gmail.com>
2023-03-24 20:23:54 +0300

    add if else

---
+++ exmpl005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("write your name, please");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "kim")
+{
+  Console.WriteLine("yes you are jim!");
+}
+else
+{
+  Console.WriteLine("hello, ");
+  Console.WriteLine(username);
+}
